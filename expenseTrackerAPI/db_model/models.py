85fe1ab870e172b33b5825d4ad541e0e7326e595
Piotr Czarnecki <czarnecki.devv@gmail.com>
2025-08-27 17:27:22 +0200

    feature/models for user and expense objects
    Create model for User and Expense table.
    Create serializer for Expense model.

--- expenseTrackerAPI/db_model/models.py
+++ expenseTrackerAPI/db_model/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=20)
+    email = models.CharField(max_length=50, unique=True)
+    password = models.CharField(max_length=20)
+
+    expenses = models.JSONField(default=list)
+
+    token = models.CharField(max_length=50)
+
+    createdAt = models.DateField(auto_now_add=True)
+
+class Expense(models.Model):
+    title = models.CharField(max_length=50)
+    amount = models.IntegerField()
+    categories = models.JSONField(default=list)
+
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
---
+++ expenseTrackerAPI/expenseTrackerAPIViews/serializers.py
@@ -0,0 +1,7 @@
+from db_model.models import Expense
+from rest_framework.serializers import ModelSerializer
+
+class ExpenseSerializer(ModelSerializer):
+    class Meta:
+        model = Expense
+        fields = '__all__'
