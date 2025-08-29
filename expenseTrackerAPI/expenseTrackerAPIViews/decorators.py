74e4cc62c5e3f9b2ab753cfa0fac12a26608a213
Piotr Czarnecki <czarnecki.devv@gmail.com>
2025-08-29 10:36:43 +0200

    feature/add check_token decorator
    Add decorator that finds user based on sent token.

--- expenseTrackerAPI/expenseTrackerAPIViews/decorators.py
+++ expenseTrackerAPI/expenseTrackerAPIViews/decorators.py
@@ -1,3 +1,9 @@
+from rest_framework import status as st
+from rest_framework.response import Response
+
+from db_model.models import User
+from views.userViews import hash_string
+
 from functools import wraps
 import json
 
@@ -6,6 +12,24 @@ def get_data(view):
     def wrapper(request):
         if '_content' in request.data:
             request.data = json.loads(request.data['_content'])
+
+        return view(request)
+
+    return wrapper
+
+def check_token(view):
+    @wraps(view)
+    def wrapper(request):
+        if request.data.get('token') == None:
+            return Response({'error': 'Token was not sent'}, status=st.HTTP_400_BAD_REQUEST)
+
+        token = request.data['token']
+        user = User.objects.filter(token=hash_string(token)).first()
+
+        if user is None:
+            return Response({'error': 'User with this token was not found. Maybe try logging in'}, status=st.HTTP_404_NOT_FOUND)
+        
+        request.user = user
 
         return view(request)
 
