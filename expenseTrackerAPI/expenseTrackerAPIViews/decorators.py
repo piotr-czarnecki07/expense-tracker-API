81ad1896b5f81c3a9d78388ada6a2938e4662348
Piotr Czarnecki <czarnecki.devv@gmail.com>
2025-08-29 12:02:25 +0200

    fix/add execption handling to check_token
    Handle exception when token that was sent is too long.

--- expenseTrackerAPI/expenseTrackerAPIViews/decorators.py
+++ expenseTrackerAPI/expenseTrackerAPIViews/decorators.py
@@ -1,5 +1,6 @@
 from rest_framework import status as st
 from rest_framework.response import Response
+from django.core.exceptions import ValidationError
 
 from db_model.models import User
 
@@ -24,7 +25,12 @@ def check_token(view):
             return Response({'error': 'Token was not sent'}, status=st.HTTP_400_BAD_REQUEST)
 
         token = request.data['token']
-        user = User.objects.filter(token=hash_string(token)).first()
+
+        try:
+            user = User.objects.filter(token=hash_string(token)).first()
+
+        except ValidationError:
+            return Response({'error': 'Token is too long'}, status=st.HTTP_400_BAD_REQUEST)
 
         if user is None:
             return Response({'error': 'User with this token was not found. Maybe try logging in'}, status=st.HTTP_404_NOT_FOUND)
